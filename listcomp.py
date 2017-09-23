65f6e0bb281bf6f8b0fbe591921a3f8e94d6aedd
Raj <rjshekar90@gmail.com>
2017-09-23 19:15:12 -0400

    code added

--- listcomp.py
+++ listcomp.py
@@ -1,2 +1,15 @@
 
 #liscomp in python
+
+#py 3
+#nested list comp
+
+mat = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9], ]
+
+for i in range(len(mat)):
+    for row in mat:
+        print(row[i], end = " ")
+    print()
